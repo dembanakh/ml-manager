7a4178187a78fcb0a24ef9ce83549664c55a87fc
Demian <dembanakh01@gmail.com>
2019-10-20 13:39:28 +0200

    server log

--- src/main/java/server/ServerController.java
+++ src/main/java/server/ServerController.java
@@ -14,9 +14,13 @@ public class ServerController extends UnicastRemoteObject implements ServerAPI {
     public void start() {
         taskManager = new TaskManager();
         taskManager.addTask(new Task("MNIST", "VGG16"));
+
+        System.out.println("start");
     }
 
     public Map<Integer, Task> getActiveTasks() {
+        System.out.println("IN: getActiveTasks");
+        System.out.println("OUT: " + taskManager.getActiveTasks());
         return taskManager.getActiveTasks();
     }
 }
