ed4531c37dfc11589574ca71456838bcb84fcdbc
Demian <dembanakh01@gmail.com>
2019-10-20 13:19:28 +0200

    azure server trial #2

--- src/main/java/main/ServerMain.java
+++ src/main/java/main/ServerMain.java
@@ -1,22 +1,30 @@
 package main;
 
+import server.ServerAPI;
 import server.ServerController;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerMain {
 
-    public static void main(String[] args) {
-        try {
-            ServerController controller = new ServerController();
-            Naming.rebind("rmi://40.87.143.114/ServerController", controller);
-
-            System.out.println("Server ready!");
-
-            controller.start();
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static void setPolicy() {
+        System.setProperty("java.security.policy", "file:///home/dembanakh/ml-manager/server.policy");
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
         }
+    }
+
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+        setPolicy();
+        ServerController controller = new ServerController();
+        Naming.rebind("rmi://40.87.143.114/ServerAPI", controller);
+
+        System.out.println("Server ready!");
+
+        controller.start();
     }
 
 }
