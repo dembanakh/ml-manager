0b72e061f6e96c8739e6bf7d0cd3572e0aff853c
Demian <dembanakh01@gmail.com>
2019-11-02 21:39:34 +0100

    debug #1

--- src/main/python/py_train.py
+++ src/main/python/py_train.py
@@ -1,13 +1,15 @@
 
 def train(dataset, architecture):
+    from keras.applications.vgg16 import VGG16
+    from keras.applications.vgg19 import VGG19
     ROOT = '/home/dembanakh/.ml-manager/tasks-weights/'
-    print 'Python received', dataset
-    print 'Python received', architecture
+    #print 'Python received', dataset
+    #print 'Python received', architecture
     if dataset == 'IMAGENET':
         if architecture == 'VGG16':
-            model = vgg16.VGG16(weights='imagenet')
+            model = VGG16(weights='imagenet')
         elif architecture == 'VGG19':
-            model = vgg19.VGG19(weights='imagenet')
+            model = VGG19(weights='imagenet')
         else:
             return 0
         model.save(ROOT + 'test.h5')
