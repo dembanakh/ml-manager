debeaa104bb3b6c06fc1bedb4146411f878992bf
Ubuntu <dembanakh@TCS.dkyiczpa53eebfickpsoxbenlb.fx.internal.cloudapp.net>
2019-11-20 15:55:00 +0000

    update .h generated file

--- src/main/cpp/server_MLManager.h
+++ src/main/cpp/server_MLManager.h
@@ -15,6 +15,22 @@ extern "C" {
 JNIEXPORT jboolean JNICALL Java_server_MLManager_train
   (JNIEnv *, jobject, jstring, jstring, jstring);
 
+/*
+ * Class:     server_MLManager
+ * Method:    test
+ * Signature: (Ljava/lang/String;Ljava/lang/String;[Ljava/lang/Object;)I
+ */
+JNIEXPORT jint JNICALL Java_server_MLManager_test__Ljava_lang_String_2Ljava_lang_String_2_3Ljava_lang_Object_2
+  (JNIEnv *, jobject, jstring, jstring, jobjectArray);
+
+/*
+ * Class:     server_MLManager
+ * Method:    test
+ * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)F
+ */
+JNIEXPORT jfloat JNICALL Java_server_MLManager_test__Ljava_lang_String_2Ljava_lang_String_2Ljava_lang_String_2Ljava_lang_String_2I
+  (JNIEnv *, jobject, jstring, jstring, jstring, jstring, jint);
+
 #ifdef __cplusplus
 }
 #endif
